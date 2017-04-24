edf8d4aa36bc6376fc0559098512dda6493e6f79
uneeb123 <uneeb@c4b301d42a53.ant.amazon.com>
2017-04-23 20:18:30 -0700

    First

--- server.rb
+++ server.rb
@@ -7,40 +7,37 @@ require 'mongoid'
 Mongoid.load! "mongoid.config"
 
 # Models
-class Book
+class Location
   include Mongoid::Document
 
-  field :title, type: String
-  field :author, type: String
-  field :isbn, type: String
+  field :user, type: String
+  field :longitude, type: Float
+  field :latitude, type: Float
 
-  validates :title, presence: true
-  validates :author, presence: true
-  validates :isbn, presence: true
+  validates :user, presence: true
+  validates :longitude, presence: true
+  validates :latitude, presence: true
 
-  index({ title: 'text' })
-  index({ isbn: 1 }, { unique: true, name: "isbn_index" })
+  index({ user: 'text' }, { unique: true })
 
-  scope :title, -> (title) { where(title: /^#{title}/) }
-  scope :isbn, -> (isbn) { where(isbn: isbn) }
-  scope :author, -> (author) { where(author: author) }
+  scope :user, -> (user) { where(user: /^#{user}$/) }
 end
 
 # Serializers
-class BookSerializer
+class LocationSerializer
 
-  def initialize(book)
-    @book = book
+  def initialize(loc)
+    @loc = loc
   end
 
   def as_json(*)
     data = {
-      id: @book.id.to_s,
-      title: @book.title,
-      author: @book.author,
-      isbn: @book.isbn
+      id: @loc.id.to_s,
+      user: @loc.user,
+      longitude: @loc.longitude,
+      latitude: @loc.latitude
     }
-    data[:errors] = @book.errors if @book.errors.any?
+    data[:errors] = @loc.errors if @loc.errors.any?
     data
   end
 
@@ -48,7 +45,7 @@ end
 
 # Endpoints
 get '/' do
-  'Welcome to BookList!'
+  'Welcome to Location Services!'
 end
 
 namespace '/api/v1' do
@@ -70,50 +67,41 @@ namespace '/api/v1' do
       end
     end
 
-    def book
-      @book ||= Book.where(id: params[:id]).first
+    def loc
+      @loc ||= Location.where(id: params[:id]).first
     end
 
-    def halt_if_not_found!
-      halt(404, { message: 'Book Not Found'}.to_json) unless book
+    def halt_if_not_found!(loc)
+      halt(404, { message: 'User Not Found'}.to_json) unless loc
     end
 
-    def serialize(book)
-      BookSerializer.new(book).to_json
+    def serialize(loc)
+      LocationSerializer.new(loc).to_json
     end
   end
 
-  get '/books' do
-    books = Book.all
+  get '/location' do
+    locs = Location.all
 
-    [:title, :isbn, :author].each do |filter|
-      books = books.send(filter, params[filter]) if params[filter]
+    [:user, :longitude, :latitude].each do |filter|
+      locs = locs.send(filter, params[filter]) if params[filter]
     end
 
-    books.map { |book| BookSerializer.new(book) }.to_json
+    locs.map { |loc| LocationSerializer.new(loc) }.to_json
   end
 
-  get '/books/:id' do |id|
-    halt_if_not_found!
-    serialize(book)
-  end
-
-  post '/books' do
-    book = Book.new(json_params)
-    halt 422, serialize(book) unless book.save
-    response.headers['Location'] = "#{base_url}/api/v1/books/#{book.id}"
+  post '/location' do
+    loc = Location.new(json_params)
+    halt 422, serialize(loc) unless loc.save
+    response.headers['Location'] = "#{base_url}/api/v1/location/#{loc.id}"
     status 201
   end
 
-  patch '/books/:id' do |id|
-    halt_if_not_found!
-    halt 422, serialize(book) unless book.update_attributes(json_params)
-    serialize(book)
-  end
-
-  delete '/books/:id' do |id|
-    book.destroy if book
-    status 204
+  patch '/location/:user' do |user|
+    loc = Location.where(:user_id => user)
+    halt_if_not_found! loc
+    halt 422, serialize(loc) unless loc.update_attributes(json_params)
+    serialize(loc)
   end
 
 end
